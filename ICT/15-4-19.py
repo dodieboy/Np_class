c79d386d52047d1433bde106d44ec552837e4fc5
Alan <shun.shun094@gmail.com>
2019-04-22 13:32:26 +0800

    22/4/2019 ICT Class
    -added 22/4/2019 class
    -compress 15/4/2019 class python file to 1 file
    -added Coursemology ICT file

--- ICT/15-4-19.py
+++ ICT/15-4-19.py
@@ -1,4 +1,29 @@
+#sum in python
+'''
 weight = float(input("Weight:"))
 height = float(input("height"))
 total = weight + height
 print(total)
+'''
+
+#cal monthly salary
+'''
+import math
+
+age= 17
+pi= 3.14
+print(type(age),age)
+print(type(pi),pi)
+
+print(math.pi)
+
+salary = float(input('Salary: ')) #float convert text(string) to number with decimal place
+bonus = float(input('Bouns: '))
+Income = salary * 12 + bonus
+print('Your monthly is $' + str(Income)) #'' or "" is use to define string
+print('Your monthly is $', Income) #we can use + or , to more than one value but , will auto add a space and auto convert indentifier/var
+'''
+
+
+# # is comment
+''' is also comment '''
---
+++ ICT/22-4-19.py
@@ -0,0 +1,77 @@
+#cal BMI
+'''
+weight = float(input("Enter your weight (in KG):"))
+height = float(input("Enter your height (in m):"))
+
+bmi = weight / (height + height)
+
+print("Your BMI is " + str(bmi))
+'''
+
+#print and loop
+'''
+a = "Hip Hip Hurray "
+b = "Welcome to ICT"
+c = 0
+
+print(a + a)
+# print(b)
+# print(b)
+# print(b)
+
+#print(3 * b + "\n")
+
+while(c<3):
+    print(b)
+    c+=1
+'''
+
+#cal GST
+'''
+Price = float(input('Price: '))
+GST = float(input('GST: '))
+
+Cost = Price /100 * (100+GST)
+    #or
+    #Cost = Price + GST / 100 * Price
+
+print("Toal cost is: " Str(Cost)
+'''
+
+#cal final mark
+'''
+print("Key in your mark for:")
+CT = float(input('Common Test: '))
+Ass = float(input('Assignment: '))
+CA = float(input('Continuous assessment: '))
+
+final = 0.3 * CT + 0.3 * Ass + 0.4 * CA
+
+print("Your final mark is " + str(final))
+'''
+
+#operators
+'''
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print('division', a / b)
+print('floor division', a//b)
+print(a % b)
+print(a ** b)
+'''
+
+#Algebraic Expression to python
+'''
+    #convert y = ax^2 + bx + c
+    y = (a * x * x) + (b * x) +c
+
+    #convert y= ((ax)^3 + bx^2 + cx) / 4
+    y = ((a * x * x * x) + (b * x * x) + (c * x)) / 4
+
+    #convert p = 1/3 + rs - uv
+    p = ((1/3) + (r * s) - (u * v))
+'''
+
