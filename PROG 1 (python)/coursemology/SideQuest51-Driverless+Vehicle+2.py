f53011038e3d24f8ee1fc63ddae5c134fa3d4a3b
dodieboy <dodieboy@users.noreply.github.com>
2019-05-27 09:02:48 +0800

    coursemology side quest 5.1 part 2

--- PROG 1 (python)/coursemology/SideQuest51-Driverless+Vehicle+2.py
+++ PROG 1 (python)/coursemology/SideQuest51-Driverless+Vehicle+2.py
@@ -16,8 +16,6 @@
 
 
 
-
-
 #Important Notes
 #===============
 #1) Comment out ALL input prompts before submitting.
@@ -31,22 +29,40 @@
 #======================
 
 #Prompt user to enter a minimum of 5 round trip times separated with commas
-roundTrips =
-
+#roundTrips =
 
 #Check closest object
 def fail_safety_distance(roundTrips):
     #Perform the parsing of roundTrips input here
-
+    roundTrips = roundTrips.replace(' ', '')
+    roundTrips = roundTrips.split(',')
+    i = 0
+    oj_index = 0
+    while i < 5:
+        if len(roundTrips) < 5:
+            closestObject = [-1, -1]
+        else:
+            short = roundTrips[0]
+            for x in range(len(roundTrips)):
+                if short > roundTrips[x]:
+                    short = roundTrips[x]
+                    oj_index = x
+            distance = 344 * (float(short) / 2)
+            closestObject = [oj_index, distance]
+        i += 1
+    
+    if distance > 50:
+        tooNear = False
+    else:
+        tooNear = True
             
     print('Object detected within safety distance:',tooNear) #Modify to display if there is an object that is too near
     
     return tooNear#Do not remove this line
 
-    
 #Do not remove the next line
 fail_safety_distance(roundTrips)
-
+#fail_safety_distance('0.5, 2.1, 0.3, 1.8, 2.3')
 
 #input 0.5,2.1,0.3,1.8,2.3  output False
 #input 0.1,2.0,1.6,.5,1     output True
--- PROG 1 (python)/coursemology/SideQuest51-Driverless+Vehicle.py
+++ PROG 1 (python)/coursemology/SideQuest51-Driverless+Vehicle.py
@@ -24,7 +24,6 @@
 
 
 
-
 #Important Notes
 #===============
 #1) Comment out ALL input prompts before submitting.
@@ -40,13 +39,13 @@
 #Prompt user to enter a minimum of 5 round trip times separated with commas
 #roundTrips = 
 
-
 #Check closest object
 def closest_object(roundTrips):
     #Perform the parsing of roundTrips input here
     roundTrips = roundTrips.replace(' ', '')
     roundTrips = roundTrips.split(',')
     i = 0
+    oj_index = 0
     while i < 5:
         if len(roundTrips) < 5:
             closestObject = [-1, -1]
@@ -55,17 +54,15 @@ def closest_object(roundTrips):
             for x in range(len(roundTrips)):
                 if short > roundTrips[x]:
                     short = roundTrips[x]
+                    oj_index = x
             distance = 344 * (float(short) / 2)
-            closestObject = [float(short), distance]
+            closestObject = [oj_index, distance]
         i += 1
-
-    
 
     print('The closest object is',closestObject) #Modify to display the closest object
     
     return closestObject#Do not remove this line
 
-    
 #Do not remove the next line
 closest_object(roundTrips)
 #closest_object('0.1,2.0,1.6,0.05')
