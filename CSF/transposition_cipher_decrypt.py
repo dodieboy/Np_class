94687a4113da3abde91a2163fe50d65af8bbf4f8
dodieboy <dodieboy@users.noreply.github.com>
2019-10-15 00:04:26 +0800

    Found a bug Columnar Transposition
    - temp fix for come plaintext cannot be decrypt with the current code

--- CSF/transposition_cipher_decrypt.py
+++ CSF/transposition_cipher_decrypt.py
@@ -3,6 +3,9 @@ var1 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
 inputs = input('Plaintext: ').replace(' ','').upper()
 key = input('Key: ').replace(' ','').upper()
 col = math.ceil(len(inputs)/len(key))
+advancedMode = False
+if len(inputs)/len(key) != col:
+    advancedMode = True
 #asign number to key
 newKey, temp = [], 0
 for i in range(len(key)):
@@ -23,24 +26,30 @@ for i in range(col):
     reorder.append([])
     for j in range(len(newKey)):
         reorder[i].append("")
-#formatting
-temp = 0
-for i in range(len(newKey)):
-    for j in range(col):
-        if temp >= len(inputs):
-            break
-        newInputs[j].append(inputs[temp])
-        temp += 1
-#reordering
-for i in range(len(newKey)):
-    for j in range(len(newKey)):
-        if newKey[j] == i+1:
-            for k in range(col):
-                reorder[k][i] = newInputs[k][j]
-            break
-#output
-output = ""
-for i in range(len(reorder)):
-    for j in range(len(reorder[i])):
-        output += reorder[i][j]
-print(output)
+if advancedMode == False:
+    #formatting
+    temp = 0
+    for i in range(len(newKey)):
+        for j in range(col):
+            if temp >= len(inputs):
+                break
+            newInputs[j].append(inputs[temp])
+            temp += 1
+    #reordering
+    for i in range(len(newKey)):
+        for j in range(len(newKey)):
+            if newKey[j] == i+1:
+                for k in range(col):
+                    if j >= len(newInputs[k]):
+                        pass
+                    else:
+                        reorder[k][i] = newInputs[k][j]
+                break
+    #output
+    output = ""
+    for i in range(len(reorder)):
+        for j in range(len(reorder[i])):
+            output += reorder[i][j]
+    print(output)
+else:
+    print("Sorry, we do not support this type of decryption yet")
