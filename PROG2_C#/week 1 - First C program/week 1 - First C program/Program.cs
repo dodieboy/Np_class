467520c71a4c5b8b65d4bc043fea398772d145d2
dodieboy <dodieboy@users.noreply.github.com>
2019-10-17 13:42:44 +0800

    Prog 2 week 1
    -rename prog 1
    -add prog2 folder
    -week 1 work (WIP)

---
+++ PROG2_C#/week 1 - First C program/week 1 - First C program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10;
+            double y = 3;
+            Console.WriteLine(x / y);
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PROG2_C#/weel 1 - BMI Calculator/BMI Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bmi;
+            double weight;
+            double height;
+
+            Console.Write("Please enter your weight in kg: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter your height in meters: ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            bmi = weight / (height * height);
+
+            Console.WriteLine("Your BMI is " + bmi);
+            Console.Read();
+        }
+    }
+}
