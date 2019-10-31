0c745f0294ef5925adcb27d4781b6476e8153b12
dodieboy <dodieboy@users.noreply.github.com>
2019-10-31 10:33:14 +0800

    Prog2 week 3: part 1

---
+++ PROG2_C#/CashCardApp/CashCardApp/CashCard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashCardApp
+{
+    class CashCard
+    {
+        private string id;
+        private double balance;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public CashCard(string i, double b)
+        {
+            Id = i;
+            Balance = b;
+        }
+        public void TopUp(double b)
+        {
+            Balance += b;
+        }
+        public bool Deduct(double b)
+        {
+            if(Balance - b < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ PROG2_C#/CashCardApp/CashCardApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CashCardApp.CashCard;
+
+namespace CashCardApp
+{
+    class Program
+    {
+        static void InitCardList(List<CashCard> cardList)
+        {
+            cardList.Add(new CashCard("001", 25));
+            cardList.Add(new CashCard("002", 25));
+            cardList.Add(new CashCard("003", 30));
+            cardList.Add(new CashCard("004", 30));
+            cardList.Add(new CashCard("005", 50));
+        }
+        static void Main(string[] args)
+        {
+            List<CashCard> cardlist = new List<CashCard> {};
+            InitCardList(cardlist);
+
+            Console.WriteLine("{0, 3} {1, 3}", "ID", "Balance");
+            for (int i = 0; i < cardlist.Count; i++)
+            {
+                Console.WriteLine("{0, 3} {1, 3}",cardlist[i].Id, cardlist[i].Balance);
+            }
+
+            Console.WriteLine("\nEnter Cash Card ID: ");
+            string cardId = Console.ReadLine();
+            CashCard cardFind = cardlist.Find(x => x.Id == cardId);
+            if(cardFind == null)
+            {
+                Console.WriteLine("You have key a invaild Cash Card ID");
+            }
+            else
+            {
+                Console.WriteLine("There is ${0} in this Cash Card", cardFind.Balance);
+
+                Console.WriteLine("Amount to topup: ");
+                int topUp = Int32.Parse(Console.ReadLine());
+
+                cardFind.TopUp(topUp);
+                cardFind = cardlist.Find(x => x.Id == cardId);
+                Console.WriteLine("\n${0} is added to your card.\nThere is ${1} in this Cash Card now", topUp.ToString(), cardFind.Balance);
+            }
+            Console.ReadLine();
+        }
+    }
+}
