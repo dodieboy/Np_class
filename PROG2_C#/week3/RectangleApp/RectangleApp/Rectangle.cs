cd5f55e0481469df4ab16eb8a4e8bec16bad5008
dodieboy <dodieboy@users.noreply.github.com>
2019-11-01 10:07:23 +0800

    FED week3: minor change

---
+++ PROG2_C#/week3/RectangleApp/RectangleApp/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleApp
+{
+    class Rectangle
+    {
+        private double width;
+        private double height;
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public Rectangle(double w, double h)
+        {
+            Height = h;
+            Width = w;
+        }
+    }
+}
