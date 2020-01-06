7beb65eddba4261a07450bd0d1b9fbf5bf0c0674
dodieboy <dodieboy@users.noreply.github.com>
2020-01-06 10:19:08 +0800

    PROG2: week 8

---
+++ PROG2_C#/week8/ShippingApp/ShippingApp/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingApp
+{
+    class Customer
+    {
+        private string name;
+        private string tel;
+        private ShippingAddr addr;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Tel
+        {
+            get { return tel; }
+            set { tel = value; }
+        }
+        public ShippingAddr Addr
+        {
+            get { return addr; }
+            set { addr = value; }
+        }
+        public Customer() { }
+        public Customer(string n, string t, ShippingAddr a)
+        {
+            Name = n;
+            Tel = t;
+            Addr = a;
+        }
+        public override string ToString()
+        {
+            return String.Format("Name: {0} Tel: {1} Address: ({2})", this.Name, this.Tel, this.Addr);
+        }
+    }
+}
---
+++ PROG2_C#/week8/ShippingApp/ShippingApp/ShippingAddr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingApp
+{
+    class ShippingAddr
+    {
+        private string country;
+        private string street;
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+        public ShippingAddr() { }
+        public ShippingAddr(string c, string s)
+        {
+            Country = c;
+            Street = s;
+        }
+        public override string ToString()
+        {
+            return String.Format("Country: {0} Street: {1}", this.Country, this.Street);
+        }
+    }
+}
