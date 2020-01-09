cafb7b91dcc36078b2e8e07493ceaf67dfa34464
dodieboy <dodieboy@users.noreply.github.com>
2020-01-09 11:54:58 +0800

    FED: week 13; better naming

--- FED_HTML/Week13Practical/js/order.js
+++ FED_HTML/Week13Practical/js/order.js
@@ -3,10 +3,10 @@ var now = new Date();
 //for today date
 today = now.toISOString().slice(0, 10);
 document.getElementById("date").setAttribute("min", today);
-//for date 2 week later
+//for a month later
 now.setMonth(now.getMonth() + 1);
-aWeekLater = now.toISOString().slice(0, 10);
-document.getElementById("date").setAttribute("max", aWeekLater);
+aMonthLater = now.toISOString().slice(0, 10);
+document.getElementById("date").setAttribute("max", aMonthLater);
 
 //form
 var item = [["Black bean purses", 3.95], ["Southwestern Napoleons", 7.95], ["Coconut-Corn Chowder", 3.95], ["Jerk Rotisserie Chicken", 12.95], ["Thai Shrimp Kebabs", 12.95], ["Pasta Puttanesca", 12.95]]
