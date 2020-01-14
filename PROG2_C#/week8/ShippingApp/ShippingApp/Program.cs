ed5312ccdb6549d21402620b8c054cc00ab32387
dodieboy <dodieboy@users.noreply.github.com>
2020-01-14 10:56:30 +0800

    PROG2: Copyright header

--- PROG2_C#/week8/ShippingApp/ShippingApp/Program.cs
+++ PROG2_C#/week8/ShippingApp/ShippingApp/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+(c) 2019 Alan Tan
+This code is licensed under MIT license (See LICENSE.txt for details)
+SPDX-Short-Identifier: MIT
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
