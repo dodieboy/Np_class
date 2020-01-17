5414a0f7b17f09a3c6b61d7577c07e733770c9ab
dodieboy <dodieboy@users.noreply.github.com>
2020-01-17 14:16:09 +0800

    PROG2: Week14

---
+++ PROG2_C#/week14/BookApp/BookApp/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+(c) 2019 Alan Tan
+This code is licensed under MIT license (See LICENSE.txt for details)
+SPDX-Short-Identifier: MIT
+*/
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> bookList;
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://ictonejourney.com");
+                Task<HttpResponseMessage> responseTask = client.GetAsync("/api/books");
+                responseTask.Wait();
+                HttpResponseMessage result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    Task<string> readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+                    string data = readTask.Result;
+                    bookList = JsonConvert.DeserializeObject<List<Book>>(data);
+                    foreach (Book b in bookList)
+                    {
+                        Console.WriteLine("{0,2}  {1,13}  {2,-65}  {3,-20}  {4,4}  {5,3}",
+                                           b.Id, b.Isbn, b.Title, b.Author, b.Pages, b.Qty);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
