bc819b9e4a67b443249d14c5ba0421a1c7d20365
dodieboy <dodieboy@users.noreply.github.com>
2021-01-02 15:07:02 +0800

    [DSA] Week 9

---
+++ DSA/Week 09/week 9/week 9/BinaryNode.h
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef int ItemType;
+
+struct BinaryNode
+{
+	ItemType item;		// data item
+	BinaryNode* left;	// pointer pointing to left subtree
+	BinaryNode* right;	// pointer pointing to right subtree
+};
